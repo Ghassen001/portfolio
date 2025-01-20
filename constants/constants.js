51c07efdbdc2537ac97e9e0cc3eb525f48a22c08
Ghassen Saahrawy <saahrawyghassen7@gmail.com>
2025-01-20 02:31:11 +0100

    Initial commit of my portfolio

---
+++ constants/constants.js
@@ -0,0 +1,68 @@
+import { BsInstagram } from "react-icons/bs";
+
+export const NAME = 'Ghassen Saahrawy';
+
+export const DESIGNATION = `web and software Developer | Open Source Contributor `;
+
+export const DETAILS = {
+  Residence: 'Tunisia',
+  City: 'Tozeur',
+  Age: '22',
+};
+
+export const SKILLS = [
+  {
+    title: 'MERN Stack Developer',
+    level: '75%',
+  },
+  {
+    title: 'React Developer',
+    level: '91%',
+  },
+  // {
+  //   title: 'Desktop Application Developer',
+  //   level: '75%',
+  // },
+  {
+    title: 'Backend Developer',
+    level: '54%',
+  },
+  {
+    title: 'Technical Analysier',
+    level: '83%',
+  },
+];
+
+export const TECH_STACK = [
+  'JavaScript',
+  'ReactJS',
+  'NextJS',
+  'TypeScript',
+  'NodeJS',
+  'Redux',
+  'Zustand',
+  'CSS',
+  'SCSS',
+  'TailwindCSS',
+  'Ant Design',
+  'Material UI',
+  'Next UI',
+  'REST API',
+  'MongoDB',
+  'MySQL',
+  'Git',
+  'GitHub',
+  'Jira',
+  'Figma',
+  'Adobe XD',
+];
+
+export const CONTACTS = {
+  EMAIL: 'saahrawyghassen7@gmail.com',
+  PHONE: '+216 48071045',
+};
+export const SOCIAL_LINKS = {
+  GITHUB: 'https://github.com/GhassenSaahrawy',
+  LINKEDIN: 'https://www.linkedin.com/in/ghassen-saahrawy-18837b208/',
+  TWITTER: 'https://x.com/saahrawy_g',
+};
